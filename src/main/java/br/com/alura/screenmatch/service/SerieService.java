02199e7fc8dce16aaf3b1314b2bda7dbc85aee18
Joanadayse <daysejoana3@gmail.com>
2024-11-03 14:26:08 -0300

    transformando a aplicação em uma API, Fornecendo dados para o front, Mapeando a pagina inicial do Screenmath

---
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -0,0 +1,50 @@
+package br.com.alura.screenmatch.service;
+
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+
+@Service
+public class SerieService {
+
+    @Autowired
+    private SerieRepository repositorio;
+
+    public List<SerieDTO> obterTodasAsSeries(){
+        return convertDados(repositorio.findAll());
+
+    }
+
+    public List<SerieDTO> obterTop5Series() {
+         return convertDados( repositorio.findTop5ByOrderByAvaliacaoDesc());
+
+    }
+
+    private List<SerieDTO> convertDados(List<Serie> series){
+        return series.stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+
+    }
+
+    public List<SerieDTO> obterLancamentos() {
+        return convertDados(repositorio.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+    }
+
+    public SerieDTO obeterPorId(Long id) {
+        Optional <Serie> serie= repositorio.findById(id);
+        if(serie.isPresent()){
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+        }
+        return null;
+    }
+}
